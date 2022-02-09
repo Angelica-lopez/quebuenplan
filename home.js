c2c861984f7cd6c82ccee6285dcfa84347823168
Angelica <angielopez4004@gmail.com>
2022-02-09 17:47:24 -0500

    home

--- home.js
+++ home.js
@@ -1,23 +1,29 @@
+const hamburguesa = document.querySelector('.hamburguesa');
+const desplegable = document.querySelector('.container-desplegable');
+let title = document.querySelector('.container__menu-title');
+let title2 = document.querySelector('.container__menu-title2');
+let countDesplegable = 0;
 
-let carousel = document.querySelector('.carousel');
-
-const milesimas = 2000;
-let contador = 0;
-
-const imagenes = [
-    'assets/toboro.jpg',
-    'assets/boulevarMonseñor.jpg',
-    'assets/anitaSuarez.jpg'
-];
-
-function pasarFoto() {
-    for(var i = 0; i <= imagenes.length-1; i++){
-        carousel.style.backgroundImage = `url(${imagenes[i]})`
+hamburguesa.addEventListener('click', function() {
+    if(countDesplegable == 0) {
+        desplegable.style.display = 'flex';
+        countDesplegable = 1;
+    }else if(countDesplegable == 1) {
+        desplegable.style.display = 'none';
+        countDesplegable = 0;
     }
+});
+
+
+function contador() {
+    title.style.display = 'none';
+    title2.style.display = 'block';
 }
 
+function contador2() {
+    title.style.display = 'block';
+    title2.style.display = 'none';
+}
 
-window.onload = function() {
-    pasarFoto();
-  };
-
+setInterval('contador()',10000);
+setInterval('contador2()',20000);
